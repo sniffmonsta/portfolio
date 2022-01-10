db62e8a402f9e9dbfd519395ab9f1eadbaae7674
Mukesh Manda <mukesh88online@gmail.com>
2022-01-10 14:57:53 +0530

    minor

--- web/src/pages/work/adaptive-learning.js
+++ web/src/pages/work/adaptive-learning.js
@@ -16,8 +16,13 @@ export default function Play() {
           text: 'Adaptive Learning'
         }
       ]} />
-      <div className="comingSoon">
-        Coming Soon
+
+      <div className="al-header">
+        <img src='../projects/p1.png' className='al-headerImage'/>
+      </div>
+      <div className="al-intro">
+        <p className="al-introTitle">The Need</p>
+        <p className="al-introText">A certain edu-tech company that has been delivering learning management systems for K-12 schools nation wide for over 10+ years, realised that creating improved content for students did not improve the rate of learning as the traditional model follows a ‘take it or leave it’ philosophy, which considered the student group to collectively have similar learning styles, levels of competence, learning needs and motivation levels.</p>
       </div>
     </Layout>
   )
