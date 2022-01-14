0619c7235f8c1920c6cd6fc821a84afd3037730a
Mukesh Manda <mukesh88online@gmail.com>
2022-01-14 11:18:28 +0530

    work page changes

--- gatsby/src/components/ProjectLink/index.js
+++ gatsby/src/components/ProjectLink/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './projectLink.css'
 import { Link } from 'gatsby';
+import _ from 'lodash';
 
 export default function ProjectLink(props) {
-    const {id, link, imageUrl, title, description} = props;
+    const {id, link, imageUrl, title, description, tags} = props;
     return (
         <Link className='projectLink' to={link}>
             <div className={id}>
@@ -11,6 +12,7 @@ export default function ProjectLink(props) {
             </div>
             <p className='projectTitle'>{title}</p>
             <p className='projectDescription'>{description}</p>
+            <p className='projectTags'>{_.map(tags, tag=><p className='projectTag'>{tag}</p>)}</p>
         </Link> 
     )
 }
