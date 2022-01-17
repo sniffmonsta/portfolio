efce4a6f13a6b2f25c98230af14115370d3b23f5
Mukesh Manda <mukesh88online@gmail.com>
2022-01-17 19:10:13 +0530

    Revert "added fonts"
    This reverts commit 13884f8174ca05f4fee40e6c8e6151592590c9c8.

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -1,4 +1,1 @@
 import "./src/styles/global/reset.css"
-import "@fontsource/open-sans"
-import "@fontsource/roboto-mono"
-import "@fontsource/roboto"
--- gatsby/src/pages/work.js
+++ gatsby/src/pages/work.js
@@ -20,24 +20,12 @@ export default function Work() {
         }
       ]} />
       <div className="projectLinksContainer">
-        <div>
-          <ProjectLink id={'p1'} imageUrl={p1} link={'adaptive-learning'} title={'Adaptive Learning'} description={'A platform for self learning that is defined by the unique learning paths students take.'} tags={['Research', 'UX', 'User Testing']} />
-        </div>
-        <div>
-          <ProjectLink id={'p2'} imageUrl={p2} link={'innovation-dash'} title={'Innovation Dash'} description={'A predictive warning system to track market innovation performance'} tags={['Research', 'UX']}/>
-         </div>
-        <div>
-          <ProjectLink id={'p3'} imageUrl={p3} link={'dot'} title={'DoT'} description={'A platform to improve social health by connecting those experiencing loneliness'} tags={['UX', 'UI']} />
-        </div>
-        <div>
-          <ProjectLink id={'p4'} imageUrl={p4} link={'moodie'} title={'Moodie'} description={'A smart haptic fidget ball which helps people deal with anxiety better'} tags={['Research', 'Product Design']}/>
-        </div>
-        <div>
-          <ProjectLink id={'p5'} imageUrl={p5} link={'krab-io'} title={'Krab.io'} description={'A smart transport management tool for any business, any size.'} tags={['Research', 'UX', 'UI']}/>
-        </div>
-        <div>
-          <ProjectLink id={'p6'} imageUrl={p6} link={'retailSystems'} title={'Retail Systems'} description={'An asset management dashboard that tracks retail machines health across the globe.'} tags={['Research', 'UX']}/>
-        </div>
+        <ProjectLink id={'p1'} imageUrl={p1} link={'adaptive-learning'} title={'Adaptive Learning'} description={'A platform for self learning that is defined by the unique learning paths students take.'} tags={['Research', 'UX', 'User Testing']} />
+        <ProjectLink id={'p2'} imageUrl={p2} link={'innovation-dash'} title={'Innovation Dash'} description={'A predictive warning system to track market innovation performance'} tags={['Research', 'UX']}/>
+        <ProjectLink id={'p3'} imageUrl={p3} link={'dot'} title={'DoT'} description={'A platform to improve social health by connecting those experiencing loneliness'} tags={['UX', 'UI']} />
+        <ProjectLink id={'p4'} imageUrl={p4} link={'moodie'} title={'Moodie'} description={'A smart haptic fidget ball which helps people deal with anxiety better'} tags={['Research', 'Product Design']}/>
+        <ProjectLink id={'p5'} imageUrl={p5} link={'krab-io'} title={'Krab.io'} description={'A smart transport management tool for any business, any size.'} tags={['Research', 'UX', 'UI']}/>
+        <ProjectLink id={'p6'} imageUrl={p6} link={'retailSystems'} title={'Retail Systems'} description={'An asset management dashboard that tracks retail machines health across the globe.'} tags={['Research', 'UX']}/>
       </div>
     </Layout>
   )
