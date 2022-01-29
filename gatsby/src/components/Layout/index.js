61e6b0c40c411fedab3ebfcce56b7f19337812f5
Mukesh Manda <mukesh88online@gmail.com>
2022-01-29 14:00:28 +0530

    removed sidebar when width<1024

--- gatsby/src/components/Layout/index.js
+++ gatsby/src/components/Layout/index.js
@@ -2,16 +2,21 @@ import React from 'react'
 import ProjectDetail from '../ProjectDetail';
 import SideBar from '../Sidebar'
 import './layout.css'
+import useWindowDimensions from '../../hooks/useWindowDimensions';
+
 
 export default function Layout(props) {
     const {children, detail} = props;
+    const { height, width } = useWindowDimensions();
+    console.log(width)
     return (
         <div className='layout'>
+            {width>=1024?(
             <div className='sideBarContainer'>
                 {detail?<ProjectDetail detail={detail}/>:<SideBar />}
-            </div>
+            </div>):null}
             <div className='contentHolder'>
-                {children}
+                {width>=320?children:null}
             </div>
         </div>
     )
